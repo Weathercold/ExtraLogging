126bbe6d8d1b384f7b0e458433ffd12a0158f167
Weathercold <weathercold.scr@gmail.com>
2021-12-24 15:43:05 -0500

    Logging 4

--- src/logging/ExtraLogFormatter.java
+++ src/logging/ExtraLogFormatter.java
@@ -3,14 +3,19 @@ package logging;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 
+import arc.util.Log;
 import arc.util.Log.DefaultLogFormatter;
+import arc.util.Strings;
 
 public class ExtraLogFormatter extends DefaultLogFormatter{
-    DateTimeFormatter timef = DateTimeFormatter.ISO_LOCAL_TIME;
+    public static DateTimeFormatter timef = DateTimeFormatter.ISO_LOCAL_TIME;
+    public static String tmpl = "[grey]@ [@]@";
 
     @Override
     public String format(String text, boolean useColors, Object... args){
-        String ftext = "[grey]" + timef.format(Instant.now()) + (text.startsWith("[EL]") ? " [blue]" : " []") + text;
-        return super.format(ftext, useColors, args);
+        text = Strings.format(text, args);
+        String ftext = Strings.format(tmpl, timef.format(Instant.now()), (text.startsWith("[EL]") ? "blue" : ""), text);
+        
+        return useColors ? Log.addColors(ftext) : Log.removeColors(ftext);
     }
 }
